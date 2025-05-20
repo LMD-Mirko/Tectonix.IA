dd86512f6b9391ac924660ab53568349cd201e59
LMD-Mirko <mirkodanielramoslimas20@gmail.com>
2025-05-20 17:19:42 -0500

    refactor: :ambulance: Actualizar dependencias, crear vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Error404/Error'
+import App from './inicio/inicio'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
